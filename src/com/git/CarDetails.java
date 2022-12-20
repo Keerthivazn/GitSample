97d583be2d1b607ddeb5c85addfd823656eade57
Keerthivazn <keerthivenkat259@gmail.om>
2022-12-20 22:20:55 +0530

    updated bt keerthi

--- src/com/git/CarDetails.java
+++ src/com/git/CarDetails.java
@@ -5,12 +5,23 @@ public class CarDetails {
 	private void mahindra() {
 		System.out.println("XUV 700");
 	}
-	
+
 	private void toyota() {
 		System.out.println("Innova Crysta");
 	}
-	
+
 	private void honda() {
-System.out.println("Amaze");
+		System.out.println("Amaze");
 	}
+
+	private void ford() {
+		System.out.println("Endeavour");
+	}
+
+
+
+
+
 }
+
+
