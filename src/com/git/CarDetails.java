2e7c5118f1eb5b422d4633eb14020b1251e8406c
Keerthivazn <keerthivenkat259@gmail.om>
2022-12-20 20:33:42 +0530

    added the project code

---
+++ src/com/git/CarDetails.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class CarDetails {
+
+	private void mahindra() {
+		System.out.println("XUV 700");
+	}
+	
+	private void toyota() {
+		System.out.println("Innova Crysta");
+	}
+}
